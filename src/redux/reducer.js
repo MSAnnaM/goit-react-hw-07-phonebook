9e9f4ecfca45d178b8120d6557789875c0e17a56
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2023-12-17 22:05:52 +0200

    update

--- src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { contactReducer } from '../redux/contactSlice';
-import { filterReducer } from '../redux/filterSlice';
-
-export const reducer = combineReducers({
-  contacts: contactReducer,
-  filter: filterReducer,
-});
